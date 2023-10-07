6252ae76967d8c12955cb3ef19eb8c6a4a782020
anxiong <xiaoxiongxiao2@gmail.com>
2023-10-07 11:26:47 +0800

    feat: Deploy Rust applications using docker

---
+++ rust-docker-example/src/main.rs
@@ -0,0 +1,21 @@
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let addr = SocketAddr::from(([0, 0, 0, 0], 5000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    println!("Request received\nSending response.");
+    Html("<h1>Hello from Rust!</h1>")
+}
