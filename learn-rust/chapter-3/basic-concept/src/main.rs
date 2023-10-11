09fce117333f7a09ae4fceeee2070ccfb6acbf90
anxiong <xiaoxiongxiao2@gmail.com>
2023-10-11 22:45:05 +0800

    feat: chapter3 fun of  the rust

--- learn-rust/chapter-3/basic-concept/src/main.rs
+++ learn-rust/chapter-3/basic-concept/src/main.rs
@@ -1,4 +1,14 @@
 fn main() {
-    const TOKEN: &str = "sfdgesdf43ytrg967uhf";
-    println!("{}", TOKEN);
+    let s = sum(12, 12);
+    println!("{}", s);
+    println!("{}", sum1(1, 2));
+}
+
+fn sum(i1: i32, i2: i32) -> i32 {
+    let s = i1 * i2;
+    s
+}
+
+fn sum1(i1: i32, i2: i32) -> i32 {
+    i1 * i2
 }
