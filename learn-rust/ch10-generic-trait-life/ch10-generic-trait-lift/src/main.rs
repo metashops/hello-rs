2aaae78b3b521ee938863bbdae28b17dea3904d3
anxiong <2652020872@qq.com>
2023-10-15 16:40:20 +0800

    feat: generic trait lifetime

---
+++ learn-rust/ch10-generic-trait-life/ch10-generic-trait-lift/src/main.rs
@@ -0,0 +1,27 @@
+use aggregator::{Summary, Tweet};
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
+
+fn largest<T>(list: &[T]) -> &T
+where
+    T: std::cmp::PartialOrd,
+{
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
